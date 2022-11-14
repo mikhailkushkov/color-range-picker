ee7424299a118eb9e7b099fe00e28367bbeac2a2
mikhailkushkov <mikhailkushkov@gmail.com>
2022-11-14 12:40:38 +0100

    improvements

--- src/colorpicker/ColorPicker.js
+++ src/colorpicker/ColorPicker.js
@@ -1,59 +1,65 @@
 export default class ColorPicker {
+  constructor(domWrapper) {
+    this.rangeInput = domWrapper.querySelector(
+      ".color-picker-color-range input"
+    );
+    this.gradientDisplay = domWrapper.querySelector(".color-picker-gradient");
+    this.rgbInput = domWrapper.querySelector(".rgb-input");
 
-    constructor(domWrapper) {
+    //jede Farbe kann max 255 schritte haben
 
-        this.rangeInput = domWrapper.querySelector('.color-picker-color-range input')
-        this.gradientDisplay = domWrapper.querySelector('.color-picker-gradient')
-        this.rgbInput = domWrapper.querySelector('.rgb-input')
+    //hier definieren wir die Farbtöne, diff - ist dynamisch und wird ersetzt durch den slider input
+    //Farbton
+    this.gradientMap = [
+      "rgb(255, DIFF, 0)",
+      "rgb(DIFF, 255, 0)",
+      "rgb(0, 255, DIFF)",
+      "rgb(0, DIFF, 255)",
+      "rgb(DIFF, 0, 255)",
+      "rgb(255, 0, DIFF)",
+    ];
 
-        //jede Farbe kann max 255 schritte haben
+    //Farbton
+    this.currentHue = "rgb(155, 0, 0)";
 
-        //hier definieren wir die Farbtöne, diff - ist dynamisch und wird ersetzt durch den slider input
-        //Farbton
-        this.gradientMap = [
-            'rgba(255, DIFF, 0)',
-            'rgba(DIFF, 255, 0)',
-            'rgba(0, 255, DIFF)',
-            'rgba(0, DIFF, 255)',
-            'rgba(DIFF, 0, 255)',
-            'rgba(255, 0, DIFF)'
-        ]
+    this.registerHandlers();
+  }
 
-        //Farbton
-        this.currentHue = 'rgba(155, 0, 0)';
+  registerHandlers() {
+    this.rangeInput.addEventListener("input", (e) =>
+      this.selectHue(e.target.value)
+    );
+  }
 
-        this.registerHandlers()
+  //value - eine Zahl wo der Slider sich gerade befindet
+  selectHue(value) {
+    value = parseInt(value);
+
+    //macht komma weg
+    //255 - maximale wert in farbspektrum
+    //in welchem farbton sind wir?
+    const currentRangeStepIndex = Math.floor(value / 255);
+
+    //in welchem schritt in farbton sind wir
+    let currentStepValue = value - 255 * currentRangeStepIndex;
+
+    //gerade zahl oder nicht gerade
+    if (currentRangeStepIndex % 2 !== 0) {
+      currentStepValue = 255 - currentStepValue;
     }
 
-    registerHandlers() {
-        this.rangeInput.addEventListener('input', (e) => this.selectHue(e.target.value))
-    }
+    this.currentHue = this.gradientMap[currentRangeStepIndex].replace(
+      "DIFF",
+      currentStepValue
+    );
+    this.render();
+  }
 
-    //value - eine Zahl wo der Slider sich gerade befindet
-    selectHue(value) {
-        value = parseInt(value)
-
-        //macht komma weg
-        //255 - maximale wert in farbspektrum
-        //in welchem farbton sind wir?
-        const currentRangeStepIndex = Math.floor(value / 255)
-
-        //in welchem schritt in farbton sind wir
-        let currentStepValue = value - (255 * currentRangeStepIndex)
-
-        //gerade zahl oder nicht gerade
-        if (currentRangeStepIndex % 2 !== 0) {
-            currentStepValue = 255 - currentStepValue
-        }
-
-        this.currentHue = this.gradientMap[currentRangeStepIndex].replace('DIFF', currentStepValue)
-        this.render()
-    }
-
-    render() {
-        this.gradientDisplay.style.backgroundImage = `linear-gradient(rgba(0,0,0,0),#000),linear-gradient(90deg,#fff, ${this.currentHue})`
-        this.rgbInput.value = this.currentHue
-    }
+  render() {
+    this.gradientDisplay.style.backgroundImage = `linear-gradient(rgba(0,0,0,0),#000),linear-gradient(90deg,#fff, ${this.currentHue})`;
+    this.rgbInput.value = this.currentHue;
+    this.rgbInput.style.color = this.rgbInput.value;
+  }
 }
 
-//statt DIFF einen canvas zu holen, und in einem canvas kann man die Farbe mit Koordinaten holen
+//TODO: statt DIFF einen canvas zu holen, und in einem canvas kann man die Farbe mit Koordinaten holen
